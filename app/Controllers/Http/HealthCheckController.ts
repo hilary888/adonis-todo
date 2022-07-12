82c45461b84a413ce6bfec57fcf71a4eaad04820
Hilary <hilarykodji@gmail.com>
2022-07-12 01:10:46 +0000

    Add health check endpoint

--- app/Controllers/Http/HealthCheckController.ts
+++ app/Controllers/Http/HealthCheckController.ts
@@ -1,3 +1,12 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import HealthCheck from '@ioc:Adonis/Core/HealthCheck'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-export default class HealthCheckController {}
+export default class HealthCheckController {
+    public async healthCheck({ response }: HttpContextContract) {
+        const report = await HealthCheck.getReport();
+
+        return report.healthy
+            ? response.ok(report)
+            : response.badRequest(report)
+    }
+}
