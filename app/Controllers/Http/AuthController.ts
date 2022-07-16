13e9a0d53c407ab94608336c41aa527e9bb56aa1
Hilary <hilarykodji@gmail.com>
2022-07-16 19:52:45 +0000

    Format codebase

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -10,171 +10,171 @@ import { DateTime } from "luxon";
 import { v4 as uuidv4 } from "uuid";
 
 export default class AuthController {
-    public async register({ request, response }: HttpContextContract) {
-        // Validate request payload
-        const payload = await request.validate(CreateUserValidator);
+  public async register({ request, response }: HttpContextContract) {
+    // Validate request payload
+    const payload = await request.validate(CreateUserValidator);
 
-        // Create new user
-        const user = await User.create(payload); // consider using a transaction instead
+    // Create new user
+    const user = await User.create(payload); // consider using a transaction instead
 
-        // Make entry into email verification token table
-        const emailVerificationObject = {
-            userId: user.id,
-            email: user.email,
-            verificationToken: uuidv4(),
-        };
+    // Make entry into email verification token table
+    const emailVerificationObject = {
+      userId: user.id,
+      email: user.email,
+      verificationToken: uuidv4(),
+    };
 
-        try {
-            await EmailVerificationToken.create(emailVerificationObject);
-            // TODO: send mail container verification details here
-        } catch (error) {
-            // If emailVerificationToken save fails, rollback user save
-            const failedUser = await User.find(user.id);
-            if (failedUser !== null) {
-                failedUser.delete();
-            }
+    try {
+      await EmailVerificationToken.create(emailVerificationObject);
+      // TODO: send mail container verification details here
+    } catch (error) {
+      // If emailVerificationToken save fails, rollback user save
+      const failedUser = await User.find(user.id);
+      if (failedUser !== null) {
+        failedUser.delete();
+      }
 
-            return response.badRequest({
-                status: "fail",
-                message: error,
-            });
-        }
-
-        const responseData = {
-            status: "success",
-            data: user.serialize(),
-        };
-        console.log("hey")
-        response.created(responseData);
+      return response.badRequest({
+        status: "fail",
+        message: error,
+      });
     }
 
-    public async login({ auth, request, response }: HttpContextContract) {
-        const { email, password, rememberMeToken } = await request.validate(LoginUserValidator);
+    const responseData = {
+      status: "success",
+      data: user.serialize(),
+    };
+    console.log("hey");
+    response.created(responseData);
+  }
 
-        try {
-            const duration: string = rememberMeToken ? "7day" : "1days";
+  public async login({ auth, request, response }: HttpContextContract) {
+    const { email, password, rememberMeToken } = await request.validate(LoginUserValidator);
 
-            const token = await auth.use("api").attempt(email, password, {
-                expiresIn: duration,
-            });
-            const responseData = {
-                status: "success",
-                data: token.toJSON(),
-            };
-            return response.ok(responseData);
-        } catch {
-            const errorResponse = {
-                status: "fail",
-                message: "Invalid credentials",
-            };
-            return response.unauthorized(errorResponse);
-        }
+    try {
+      const duration: string = rememberMeToken ? "7day" : "1days";
+
+      const token = await auth.use("api").attempt(email, password, {
+        expiresIn: duration,
+      });
+      const responseData = {
+        status: "success",
+        data: token.toJSON(),
+      };
+      return response.ok(responseData);
+    } catch {
+      const errorResponse = {
+        status: "fail",
+        message: "Invalid credentials",
+      };
+      return response.unauthorized(errorResponse);
+    }
+  }
+
+  public async logout({ auth, response }) {
+    // only accessible behind auth middleware
+    await auth.use("api").revoke();
+    return response.ok({
+      status: "success",
+      revoked: true,
+    });
+  }
+
+  public async verifyEmail({ request, response }: HttpContextContract) {
+    const { email, verificationToken } = await request.validate(EmailVerificationTokenValidator);
+    let emailVerificationTokenRecord: EmailVerificationToken;
+
+    // Find email verification token record
+    try {
+      emailVerificationTokenRecord = await EmailVerificationToken.findByOrFail("email", email);
+    } catch (error) {
+      const responseData = {
+        status: "fail",
+        message: error.message,
+      };
+      return response.notFound(responseData);
     }
 
-    public async logout({ auth, response }) {
-        // only accessible behind auth middleware
-        await auth.use("api").revoke();
-        return response.ok({
-            status: "success",
-            revoked: true,
-        });
+    // Check if provided token matches saved token
+    const isValid =
+      verificationToken === emailVerificationTokenRecord.verificationToken &&
+      emailVerificationTokenRecord !== undefined;
+    if (!isValid) {
+      return response.badRequest({
+        status: "fail",
+        message: "email verification failed due to invalid token",
+      });
     }
 
-    public async verifyEmail({ request, response }: HttpContextContract) {
-        const { email, verificationToken } = await request.validate(EmailVerificationTokenValidator);
-        let emailVerificationTokenRecord: EmailVerificationToken;
+    // Update record
+    emailVerificationTokenRecord.isVerified = true;
+    emailVerificationTokenRecord.verifiedAt = DateTime.now();
+    const verifiedEmail = await emailVerificationTokenRecord.save();
 
-        // Find email verification token record
-        try {
-            emailVerificationTokenRecord = await EmailVerificationToken.findByOrFail("email", email);
-        } catch (error) {
-            const responseData = {
-                status: "fail",
-                message: error.message,
-            };
-            return response.notFound(responseData);
-        }
+    const responseData = {
+      status: "success",
+      data: verifiedEmail,
+    };
 
-        // Check if provided token matches saved token
-        const isValid =
-            verificationToken === emailVerificationTokenRecord.verificationToken &&
-            emailVerificationTokenRecord !== undefined;
-        if (!isValid) {
-            return response.badRequest({
-                status: "fail",
-                message: "email verification failed due to invalid token",
-            });
-        }
+    return response.ok(responseData);
+  }
 
-        // Update record
-        emailVerificationTokenRecord.isVerified = true;
-        emailVerificationTokenRecord.verifiedAt = DateTime.now();
-        const verifiedEmail = await emailVerificationTokenRecord.save();
-
-        const responseData = {
-            status: "success",
-            data: verifiedEmail,
-        };
-
-        return response.ok(responseData);
+  public async forgotPassword({ request, response }) {
+    const { email } = request.qs();
+    let user: User;
+    try {
+      user = await User.findByOrFail("email", email);
+    } catch (error) {
+      return response.notFound({
+        status: "fail",
+        message: error.message,
+      });
     }
 
-    public async forgotPassword({ request, response }) {
-        const { email } = request.qs();
-        let user: User;
-        try {
-            user = await User.findByOrFail("email", email);
-        } catch (error) {
-            return response.notFound({
-                status: "fail",
-                message: error.message,
-            });
-        }
+    const passwordResetTokenEntry = {
+      userId: user.id,
+      token: uuidv4(),
+    };
 
-        const passwordResetTokenEntry = {
-            userId: user.id,
-            token: uuidv4(),
-        };
+    const result = PasswordResetToken.create(passwordResetTokenEntry);
 
-        const result = PasswordResetToken.create(passwordResetTokenEntry);
+    return response.ok({
+      status: "success",
+      data: (await result).$isPersisted,
+    });
+  }
 
-        return response.ok({
-            status: "success",
-            data: (await result).$isPersisted,
-        });
+  public async resetPassword({ request, response }: HttpContextContract) {
+    // Enable user reset password
+    const { token, newPassword } = await request.validate(ResetPasswordValidator);
+
+    let passwordResetTokenRecord: PasswordResetToken;
+    let user: User;
+
+    try {
+      passwordResetTokenRecord = await PasswordResetToken.findByOrFail("token", token);
+    } catch (error) {
+      return response.notFound({
+        status: "fail",
+        message: error.message,
+      });
     }
 
-    public async resetPassword({ request, response }: HttpContextContract) {
-        // Enable user reset password
-        const { token, newPassword } = await request.validate(ResetPasswordValidator);
-
-        let passwordResetTokenRecord: PasswordResetToken;
-        let user: User;
-
-        try {
-            passwordResetTokenRecord = await PasswordResetToken.findByOrFail("token", token);
-        } catch (error) {
-            return response.notFound({
-                status: "fail",
-                message: error.message,
-            });
-        }
-
-        try {
-            user = await User.findOrFail(passwordResetTokenRecord.userId);
-        } catch (error) {
-            return response.badRequest({
-                status: "fail",
-                message: error.message,
-            });
-        }
-
-        user.password = newPassword;
-        await user.save();
-
-        return response.ok({
-            status: "success",
-            data: { newPasswordSet: user.$isPersisted },
-        });
+    try {
+      user = await User.findOrFail(passwordResetTokenRecord.userId);
+    } catch (error) {
+      return response.badRequest({
+        status: "fail",
+        message: error.message,
+      });
     }
+
+    user.password = newPassword;
+    await user.save();
+
+    return response.ok({
+      status: "success",
+      data: { newPasswordSet: user.$isPersisted },
+    });
+  }
 }
